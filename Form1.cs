a4fa2a8a848f896d1e44fb9d17888a89866545b2
bax082024 <bax082024@gmail.com>
2024-10-14 17:12:28 +0200

    added a face

--- Form1.cs
+++ Form1.cs
@@ -18,11 +18,27 @@ public partial class Form1 : Form
         base.OnPaint(e);
 
         Graphics g = e.Graphics;
+
         g.DrawRectangle(Pens.Black, 50, 50, 200, 100);
 
         g.DrawEllipse(Pens.Blue, 300, 50, 200, 100);
 
+
+
+
+        g.DrawEllipse(Pens.Black, 100, 200, 200, 200);
+
+        g.DrawEllipse(Pens.Black, 140, 250, 40, 40);
+
+        g.DrawEllipse(Pens.Black, 220, 250, 40, 40);
+
+        g.DrawLine(Pens.Black, 150, 330, 250, 330);
+
+        
+
         Font font = new Font("Arial", 16);
-        g.DrawString("Hello, Graphics!", font, Brushes.Red, 150, 200);
+        g.DrawString("Hello, Graphics!", font, Brushes.Red, 150, 400);
+
+        g.DrawString("This is Fun!", font, Brushes.Red, 150, 450);
     }
 }
